1398b283851f0b7d860746b8d830e65ecc160e84
Hyeon Kim <simnalamburt@gmail.com>
2014-08-24 15:45:03 -0700

    Separated source codes from build system

--- slm/Gulpfile.js
+++ slm/Gulpfile.js
@@ -3,16 +3,17 @@
 var gulp = require('gulp');
 var slm = require('gulp-slm');
 
-gulp.task('build', function() {
-  return gulp.src('main.slm')
-    .pipe(slm({
-      locals: {
-        data: 100
-      }
-    }))
-    .pipe(gulp.dest('.'));
+gulp.task('slm', function() {
+  var data = {
+    title: 'Hello, world!',
+    text: 'Hello world example for slm template.',
+  };
+
+  return gulp.src('./src/*.slm')
+    .pipe(slm({ locals: data }))
+    .pipe(gulp.dest('./build/'));
 });
 
 gulp.task('default', function() {
-  gulp.start('build');
+  gulp.start('slm');
 });
