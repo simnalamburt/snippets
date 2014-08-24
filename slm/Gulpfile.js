9478f445bdd0c0bd8282240f5bed88ff3b5a1784
Hyeon Kim <simnalamburt@gmail.com>
2014-08-23 21:00:33 -0700

    Test gulp compilation

--- slm/Gulpfile.js
+++ slm/Gulpfile.js
@@ -1,9 +1,14 @@
 'use strict';
 
 var gulp = require('gulp');
+var slm = require('gulp-slm');
+
+gulp.task('build', function() {
+  return gulp.src('main.slm')
+    .pipe(slm())
+    .pipe(gulp.dest('.'));
+});
 
 gulp.task('default', function() {
-  console.log('');
-  console.log('There\'s nothing to do');
-  console.log('');
+  gulp.start('build');
 });
