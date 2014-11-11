3d768a4453f21fc0a73ad613c3a1c1ca0eed34f7
Hyeon Kim <simnalamburt@gmail.com>
2014-11-11 23:58:57 +0900

    )\n{ -> ) {

--- main.c
+++ main.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 
 
-int main(void)
-{
+int main(void) {
   char input[1001], delims[101], *tokens[1001], *temp;
   int w;
 
-  for(w=0; w<1001; w++)
-  {
+  for(w=0; w<1001; w++) {
     input[w]='0';
   }
 
@@ -16,28 +14,22 @@ int main(void)
 
   int i, j, k=1;
   tokens[0]=&input[0];
-  for(i=0; i<1000; i++)
-  {
-    for(j=0; j<100; j++)
-    {
+  for(i=0; i<1000; i++) {
+    for(j=0; j<100; j++) {
       if(input[i]==delims[j]){input[i]='0'; tokens[k]=&input[i+1]; k++; break;}
     }
   }
 
-  for(i=0;i<1000;i++)
-  {
+  for(i=0;i<1000;i++) {
     if(input[i]==0) {input[i]='0'; break;}
   }
 
 
-  for(i=0; i<k-2; i++)
-  {
-    for(j=i+1; j<k-1; j++)
-    {
+  for(i=0; i<k-2; i++) {
+    for(j=i+1; j<k-1; j++) {
       int q=0;
       temp = 0;
-      while(1)
-      {
+      while(1) {
         if(*tokens[i]>*tokens[j] && *tokens[i]-*tokens[j]!=32){temp=tokens[i]; tokens[i]=tokens[j]; tokens[j]=temp; break;}
         else if(*tokens[i]<*tokens[j] && *tokens[j]-*tokens[i]!=32){break;}
         else if(*tokens[i]=='0' && *tokens[j]!='0'){break;}
@@ -51,16 +43,13 @@ int main(void)
 
   char output[1001];
   int h;
-  for(h=0; h<1000; h++)
-  {
+  for(h=0; h<1000; h++) {
     output[h]='0';
   }
 
   int t=0;
-  for(i=0; i<k-1; i++)
-  {
-    for(j=0; *(tokens[i]+j)!='0'; j++)
-    {
+  for(i=0; i<k-1; i++) {
+    for(j=0; *(tokens[i]+j)!='0'; j++) {
       output[t]= *(tokens[i]+j);
       t++;
     }
@@ -69,22 +58,19 @@ int main(void)
   }
 
   t=0;
-  for(i=0; i<1000; i++)
-  {
+  for(i=0; i<1000; i++) {
     if(output[i]==' '){t++;}
     else break;
   }
 
   int u=0, d=999;
-  for(u=999; u>=0; --u)
-  {
+  for(u=999; u>=0; --u) {
     if(output[u]=='0'){--d;}
     else break;
   }
 
   int e;
-  for(e=t; e<d; e++)
-  {
+  for(e=t; e<d; e++) {
     printf("%c", output[e]);
   }
 
