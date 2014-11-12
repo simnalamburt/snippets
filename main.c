737f359f9b1c80f0bf93aef95c09d68ea1314ca1
Hyeon Kim <simnalamburt@gmail.com>
2014-11-13 03:04:31 +0900

    토큰 나누는 코드 최적화

--- main.c
+++ main.c
@@ -46,10 +46,15 @@ int main(void) {
   // 문자열 input 안의 delimeter들을 전부 NULL로 치환
   // fgets 쓰기때문에 delims 엔 자동으로 '\n'이 들어있음
   //
+  // NULL로 치환하며, 동시에 tokens에 치환된 위치를 저장
+  //
+  char* tokens[501] = { input };
+  int _count = 1;
   for (i = 0; input[i]; ++i) {
     for (j = 0; delims[j]; ++j) {
       if (input[i] == delims[j]) {
         input[i] = '\0';
+        tokens[_count++] = &input[i + 1];
         break;
       }
     }
@@ -57,22 +62,12 @@ int main(void) {
   const int input_size = i - 1; // '\n' 제외
 
   //
-  // NULL로 구분된 입력들의 각 시작점을 tokens[]에 저장
-  // 이때 빈 토큰은 제외
+  // 빈 token 제외하기
   //
-  char* tokens[501];
-  int _count = 0;
-  bool _last_state = false; // 마지막으로 읽은 글자가 NULL이었을경우 false
-                            //                otherwise              true
-  for (i = 0; i < input_size; ++i) {
-    bool _current_state = !!input[i]; // 현재 글자가 NULL일 경우 false
-                                      //        otherwise        true
+  for (i = 0; i < _count; ++i) {
+    if (tokens[i][0] != '\0') continue;
 
-    if (!_last_state && _current_state) {
-      tokens[_count++] = &input[i];
-    }
-
-    _last_state = _current_state;
+    tokens[i--] = tokens[--_count];
   }
   const int tokens_count = _count;
 
