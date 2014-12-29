cb46e7b5d3f839b883a2c13260701771667f2e3f
Hyeon Kim <simnalamburt@gmail.com>
2014-12-30 01:22:50 +0900

    rustc: Make match.rs simpler

--- rust/match.rs
+++ rust/match.rs
@@ -1,8 +1,9 @@
 #![feature(globs)]
 
-use self::User::*;
+use std::io::stdio::println;
+use self::Name::*;
 
-enum User {
+enum Name {
     Hyeon,
     Newnyang,
     P,
@@ -12,14 +13,20 @@ enum User {
 }
 
 fn main() {
-    let a = Newnyang;
-
-    println!("{}", match a {
+    println(match Newnyang {
         Hyeon => "김젼",
         Newnyang => "뉴냥",
         P => "포비아",
         Sgkim => "심규김",
         Sgm => "심규민",
         Gallen => "갈렌"
+    });
+
+    println(match 1000i32 {
+        0...9 => "한자리수",
+        10...99 => "두자리수",
+        100...999 => "세자리수",
+        1000...9999 => "네자리수",
+        _ => "다섯자리 이상"
     });
 }
