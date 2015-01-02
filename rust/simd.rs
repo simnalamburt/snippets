a208fb687005381d1fdfabd07310e82fc08c29a0
Hyeon Kim <simnalamburt@gmail.com>
2015-01-03 06:19:55 +0900

    rust: Update simd.rs

--- rust/simd.rs
+++ rust/simd.rs
@@ -2,14 +2,14 @@
 
 #[simd]
 #[deriving(Copy, Show)]
-#[repr(C)]
-struct i32x2(i32, i32);
+#[allow(non_camel_case_types)]
+struct f32x3(f32, f32, f32);
 
 fn main() {
-    let a = i32x2(10, 20);
-    let b = i32x2(05, 05);
+    let a = f32x3(1.0, 2.0, 3.0);
+    let b = f32x3(0.5, 1.5, 2.5);
 
-    let c = a > b;
+    let c = a - b;
 
     println!("{}", c);
 }
