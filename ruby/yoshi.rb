ce60eee048ac283a96eb50eeac0688dabf2f0c67
Hyeon Kim <simnalamburt@gmail.com>
2015-02-15 06:33:28 +0900

    ruby: yoshi.rb

---
+++ ruby/yoshi.rb
@@ -0,0 +1,10 @@
+# http://i.imgur.com/kwnQ6Iy.png
+
+class String
+  def yoshi
+    ([0xC3] + self.encode('CP949').unpack('C*') + [0xA4]).pack('C*').force_encoding('CP949').encode('UTF-8')
+  end
+end
+
+puts '아그란도시즌'.yoshi   # 첸튿溜促돕쳅制
+puts '요시초콜릿먹어'.yoshi # 첼鵝쳉珏賻뉨禿在
