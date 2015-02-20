ced6d77929c52985e558fb12a78bfb5d5cb998c7
Hyeon Kim <simnalamburt@gmail.com>
2015-02-20 10:53:37 +0900

    rust: breakable-block.rs -> iice.rs

--- rust-cargo/src/bin/iice.rs
+++ rust-cargo/src/bin/iice.rs
@@ -1,15 +1,12 @@
 #![allow(unreachable_code)]
 
+/// Immediately-invoked closure expression
 fn main() {
     println!("ok");
-
-    (|&:| {
+    (|| {
         println!("ok");
-
         return;
-
         panic!();
     })();
-
     println!("ok");
 }
