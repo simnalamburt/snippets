388fa382ba8ed239ad11c681e8aeb8d50dd47180
Hyeon Kim <simnalamburt@gmail.com>
2015-02-27 13:18:57 +0900

    rust: path.rs

---
+++ rust-cargo/src/bin/path.rs
@@ -0,0 +1,12 @@
+#![feature(env, path)]
+
+use std::env::current_exe;
+
+fn main() {
+    if let Ok(path) = current_exe() {
+        let mut a = path.clone();
+        a.push("..");
+        println!("{:?}", a);
+        println!("{:?}", (&*path).parent().unwrap());
+    }
+}
