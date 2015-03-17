a1f654ee787a76bb9643f3f01d6441c2a2507266
Hyeon Kim <simnalamburt@gmail.com>
2015-03-17 14:27:35 +0900

    c: fork.c 소스 좀더 재밌게 만듬

--- c/fork.c
+++ c/fork.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
 
-int main(void)
-{
-  printf("(%d) 포크 전\n", getpid());
-  fork();
-  printf("(%d) 포크 후\n", getpid());
-
-  return 0;
+int main() {
+    puts("ㅇㅅㅇ");
+    fork();
+    puts("ㅇㅅㅇ..");
+    fork();
+    puts("ㅇㅅㅇ....");
+    fork();
+    puts("ㅇㅅㅇ......");
+    return 0;
 }
