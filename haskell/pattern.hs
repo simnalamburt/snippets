d02311f1973fab4198150e9c85fc836e076db46d
Hyeon Kim <simnalamburt@gmail.com>
2015-03-28 18:55:35 +0900

    haskell: pattern.hs

---
+++ haskell/pattern.hs
@@ -0,0 +1,14 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import Data.Monoid
+import qualified Data.ByteString.Char8 as B
+
+spacedConcat :: [B.ByteString] -> B.ByteString
+spacedConcat [] = B.empty
+spacedConcat [b] = b
+spacedConcat bs = do
+  mconcat [head bs, " ", spacedConcat $ tail bs]
+
+main :: IO ()
+main = do
+  B.putStrLn $ spacedConcat ["I", "am", "hungry"]
