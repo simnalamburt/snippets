476232f29d54c956374daeddb6a1a26bac5bce37
Hyeon Kim <simnalamburt@gmail.com>
2015-07-28 05:24:27 +0900

    rust: utime.rs

--- rust-stable/src/bin/utime.rs
+++ rust-stable/src/bin/utime.rs
@@ -1,3 +1,20 @@
+extern crate libc;
+
+use std::ffi::CString;
+use libc::types::os::arch::posix01::utimbuf;
+use libc::funcs::posix88::unistd::utime;
+
 fn main() {
-    println!("Hello, world!");
+    let buf = utimbuf {
+        actime: 0,
+        modtime: 0,
+    };
+
+    let name = CString::new("utime").unwrap();
+
+    let ret = unsafe { utime(name.as_ptr(), &buf) };
+    if ret != 0 {
+        println!("utime: 실패!");
+        std::process::exit(1);
+    }
 }
