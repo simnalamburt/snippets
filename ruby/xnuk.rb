d2918debb0a8f5e083c62fe84563ba1f53024a13
Hyeon Kim <simnalamburt@gmail.com>
2015-08-10 19:24:37 +0900

    ruby: @xnuk

---
+++ ruby/xnuk.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/env ruby
+loop do
+  begin
+    result = gets.force_encoding('CP949').encode('UTF-8').strip
+  rescue
+    result = "\e[31mError"
+  end
+
+  puts "\e[33m#{result}\e[0m"
+  puts ''
+end
