48ee479253077f6d8fe6b7bc4544af8e4661c94f
Hyeon Kim <simnalamburt@gmail.com>
2015-10-18 22:18:31 +0900

    cpp: 오버로딩

---
+++ cpp/overloading.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+// Function overloading
+int foo(int a) { return a; }
+int foo(int a, int b) { return a + b; }
+void printf() { printf("Hello, world!\n"); }
+
+// Method overloading
+
+// Operator overloading
+
+int main() {
+  printf();
+}
