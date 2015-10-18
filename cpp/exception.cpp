3874cf17cfcb95e017bace3e7511ae4860ffd9a4
Hyeon Kim <simnalamburt@gmail.com>
2015-10-18 18:26:43 +0900

    cpp: exception.cpp

---
+++ cpp/exception.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() try {
+  string str = "5000000000";
+
+  int a = stoi(str);
+} catch (...) {
+  // Pokemon
+  cout << "다잡음" << endl;
+}
