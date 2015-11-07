ae2eb4a40fb857e09f1cdc369615ed5f2cb47b23
Hyeon Kim <simnalamburt@gmail.com>
2015-11-07 17:24:37 +0900

    ruby: curses 연습

---
+++ ruby/curses.rb
@@ -0,0 +1,20 @@
+require 'curses'
+
+Curses.init_screen
+
+wins = (0..10).map do |i|
+  Curses::Window.new(1, 0, 2 + i, 0)
+end
+
+1000.times do
+  win = wins.sample
+  win << '*'
+  win.refresh
+
+  sleep 0.001
+end
+
+msg = Curses::Window.new(1, 0, 0, 0)
+msg << 'Done!'
+msg.refresh
+msg.getch
