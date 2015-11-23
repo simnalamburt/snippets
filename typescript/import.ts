20e83db3a6a7fde979566442fd7f00b0daffe55c
Hyeon Kim <simnalamburt@gmail.com>
2015-11-23 23:45:25 +0900

    typescript: declare var require: (name: string) => any;

--- typescript/import.ts
+++ typescript/import.ts
@@ -4,7 +4,7 @@ import * as hello from './export-hello';
 console.log(hello.hello());
 
 // commonjs
-declare var require: (string) => any;
+declare var require: (name: string) => any;
 var fs = require('fs');
 
 console.log(fs.readdirSync('.'));
