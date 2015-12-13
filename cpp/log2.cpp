329572a76a5bce7e347d45c8549dabb7d7077566
Hyeon Kim <simnalamburt@gmail.com>
2015-12-13 16:24:02 +0900

    C++: log2.cpp

---
+++ cpp/log2.cpp
@@ -0,0 +1,38 @@
+#include <chrono>
+#include <iostream>
+
+using namespace std;
+using namespace chrono;
+
+static inline uint32_t log2(const uint32_t x) {
+  uint32_t y;
+  asm ("bsr %1, %0" : "=r"(y) : "r" (x));
+  return y;
+}
+
+static inline uint32_t log2_slow(uint32_t x) {
+  uint32_t ret = 0;
+  while (x >>= 1) { ++ret; }
+  return ret;
+}
+
+int main() {
+  auto t1 = high_resolution_clock::now();
+
+  for (uint32_t i = 0; i < 100000000; ++i) {
+    volatile auto _ = log2(i);
+  }
+
+  auto t2 = high_resolution_clock::now();
+
+  for (uint32_t i = 0; i < 100000000; ++i) {
+    volatile auto _ = log2_slow(i);
+  }
+
+  auto t3 = high_resolution_clock::now();
+
+  cout << "어셈 : " << duration<double>(t2 - t1).count() * 1000 << "ms\n";
+  cout << "계산 : " << duration<double>(t3 - t2).count() * 1000 << "ms\n";
+
+  return 0;
+}
