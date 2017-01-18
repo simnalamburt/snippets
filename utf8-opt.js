f57fea43ef24ad15c0b56b467c72cb9013f31fd9
Hyeon Kim <simnalamburt@gmail.com>
2017-01-18 22:38:11 +0900

    함수 최적화여부 더 잘 알 수 있게

--- utf8-opt.js
+++ utf8-opt.js
@@ -4,7 +4,6 @@
 "use strict";
 
 const h = [0, 192, 224, 240];
-
 function encode(input) {
   if (!(input === (input|0) && input >= 0 && input < 1114112)) { return []; }
 
@@ -25,27 +24,30 @@ function encode(input) {
 //
 function printStatus(fn) {
   switch(%GetOptimizationStatus(fn)) {
-  case 1:   console.log("\x1b[32mFunction is optimized\x1b[0m"); return;
-  case 2:   console.log("\x1b[32mFunction is not optimized\x1b[0m"); return;
-  case 3:   console.log("\x1b[32mFunction is always optimized\x1b[0m"); return;
-  case 4:   console.log("\x1b[32mFunction is never optimized\x1b[0m"); return;
-  case 6:   console.log("\x1b[32mFunction is maybe deoptimized\x1b[0m"); return;
-  case 7:   console.log("\x1b[32mFunction is optimized by TurboFan\x1b[0m"); return;
-  default:  console.log("\x1b[32mUnknown optimization status\x1b[0m"); return;
+  case 1:   console.log(`\x1b[32m${fn.name}() : optimized\x1b[0m`); return;
+  case 2:   console.log(`\x1b[32m${fn.name}() : not optimized\x1b[0m`); return;
+  case 3:   console.log(`\x1b[32m${fn.name}() : always optimized\x1b[0m`); return;
+  case 4:   console.log(`\x1b[32m${fn.name}() : never optimized\x1b[0m`); return;
+  case 6:   console.log(`\x1b[32m${fn.name}() : maybe deoptimized\x1b[0m`); return;
+  case 7:   console.log(`\x1b[32m${fn.name}() : optimized by TurboFan\x1b[0m`); return;
+  default:  console.log(`\x1b[32m${fn.name}() : unknown optimization status\x1b[0m`); return;
   }
+}
+function status() {
+  printStatus(encode);
 }
 
 console.log('\x1b[33mTest Result :', encode(55176), '\x1b[0m');
 console.log('\x1b[33mExpected    :', [237, 158, 136], '\x1b[0m');
 
 console.log();
-printStatus(encode);
+status();
 console.log('\x1b[33mWarming up... (1Mops)\x1b[0m');
 for (let i = 0; i < 1000000; i++) {
   const val = Math.floor(Math.random() * 0x100);
   encode(val);
 }
-printStatus(encode);
+status();
 console.log();
 
 console.time('\x1b[33mNormal UTF-8 (10Mops)\x1b[0m');
@@ -63,4 +65,4 @@ for (let i = 0; i < 10000000; i++) {
 console.timeEnd('\x1b[33mUTF-8 invalid codepoints (10Mops)\x1b[0m');
 
 console.log();
-printStatus(encode);
+status();
