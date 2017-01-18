e86234f8f742984ec4dff40e455ce4dbe2a9ffc4
Hyeon Kim <simnalamburt@gmail.com>
2017-01-18 22:19:43 +0900

    Optimization: ~600ms -> ~470ms

--- utf8-opt.js
+++ utf8-opt.js
@@ -3,23 +3,21 @@
 //
 "use strict";
 
-const u = (function () {
-  const h = [0, 192, 224, 240];
-  function t($) {
-    const cnt = $ < 128 ? 0 : $ < 2048 ? 1 : $ < 65536 ? 2 : 3;
-    const ret = new Array(cnt+1);
-    const head = h[cnt];
-    for (let i = cnt; i > 0; i--) {
-      ret[i] = 128 | $ & 63;
-      $ >>>= 6;
-    }
-    ret[0] = head | $;
-    return ret;
+const h = [0, 192, 224, 240];
+function t($) {
+  const cnt = $ < 128 ? 0 : $ < 2048 ? 1 : $ < 65536 ? 2 : 3;
+  const ret = new Array(cnt+1);
+  const head = h[cnt];
+  for (var i = cnt; i > 0; i--) {
+    ret[i] = 128 | $ & 63;
+    $ >>>= 6;
   }
-  return function ($) {
-    return $ === ($|0) && $ >= 0 && $ < 1114112 ? t($) : []
-  }
-})();
+  ret[0] = head | $;
+  return ret;
+}
+function u($) {
+  return $ === ($|0) && $ >= 0 && $ < 1114112 ? t($) : []
+}
 
 
 //
