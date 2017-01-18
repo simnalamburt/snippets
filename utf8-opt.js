25a6e0b55d865f975e1fddc2fa803f7859427d5f
Hyeon Kim <simnalamburt@gmail.com>
2017-01-19 00:29:21 +0900

    320ms

--- utf8-opt.js
+++ utf8-opt.js
@@ -1,4 +1,5 @@
 //
+// node --allow-natives-syntax utf8-opt.js
 // node --trace_opt --trace_deopt --allow-natives-syntax utf8-opt.js
 //
 "use strict";
@@ -8,38 +9,32 @@ function case0(input) {
 }
 function case1(input) {
   return [
-    192 | (input >>> 6),
-    128 | input & 63,
+    0b11000000 | (input >>> 6),
+    0b10000000 | input & 0b00111111,
   ];
 }
 function case2(input) {
   return [
-    224 | (input >>> 12),
-    128 | (input >>> 6) & 63,
-    128 | input & 63,
+    0b11100000 | (input >>> 12),
+    0b10000000 | (input >>> 6) & 0b00111111,
+    0b10000000 | input & 0b00111111,
   ];
 }
 function case3(input) {
   return [
-    240 | (input >>> 18),
-    128 | (input >>> 12) & 63,
-    128 | (input >>> 6) & 63,
-    128 | input & 63,
+    0b11110000 | (input >>> 18),
+    0b10000000 | (input >>> 12) & 0b00111111,
+    0b10000000 | (input >>> 6) & 0b00111111,
+    0b10000000 | input & 0b00111111,
   ];
 }
 
 function encode(input) {
-  if (!(input === (input|0) && input >= 0 && input < 1114112)) {
-    return [];
-  } else if (input < 128) {
-    return case0(input);
-  } else if (input < 2048) {
-    return case1(input);
-  } else if (input < 65536) {
-    return case2(input);
-  } else {
-    return case3(input);
-  }
+  if (input !== (input|0) || input < 0 || input >= 0x00110000) { return []; }
+  if (input < 2** 7) { return case0(input); }
+  if (input < 2**11) { return case1(input); }
+  if (input < 2**16) { return case2(input); }
+  return case3(input);
 }
 
 
@@ -65,13 +60,15 @@ function status() {
   printStatus(case3);
 }
 
-console.log('\x1b[33mTest Result :', encode(55176), '\x1b[0m');
-console.log('\x1b[33mExpected    :', [237, 158, 136], '\x1b[0m');
+console.log('\x1b[33mTest Result :', encode(0xD788), '\x1b[0m');
+console.log('\x1b[33mExpected    :', [0xED, 0x9E, 0x88], '\x1b[0m');
+
+var i;
 
 console.log();
 status();
 console.log('\x1b[33mWarming up... (1Mops)\x1b[0m');
-for (var i = 0; i < 1000000; i++) {
+for (i = 0; i < 1E6; ++i) {
   encode(
     Math.floor(Math.random() * 0x100) | 0
   );
@@ -80,7 +77,7 @@ status();
 console.log();
 
 console.time('\x1b[33mNormal UTF-8 (10Mops)\x1b[0m');
-for (var i = 0; i < 10000000; i++) {
+for (i = 0; i < 1E7; ++i) {
   encode(
     Math.floor(Math.random() * 0x110000) | 0
   );
@@ -88,7 +85,7 @@ for (var i = 0; i < 10000000; i++) {
 console.timeEnd('\x1b[33mNormal UTF-8 (10Mops)\x1b[0m');
 
 console.time('\x1b[33mUTF-8 invalid codepoints (10Mops)\x1b[0m');
-for (var i = 0; i < 10000000; i++) {
+for (i = 0; i < 1E7; ++i) {
   encode(
     (Math.floor(Math.random() * 0x1000) | 0) + 0x110000
   );
