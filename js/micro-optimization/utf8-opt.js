9cd952ace941054af9a7a820f02f5f0f5c9b485d
Hyeon Kim <simnalamburt@gmail.com>
2017-01-19 15:47:01 +0900

    Merge files

--- js/micro-optimization/utf8-opt.js
+++ js/micro-optimization/utf8-opt.js
@@ -1,7 +1,38 @@
+// V8 micro-optimization practice
+// ========
+// Execute with the command below to make sure that nothing is deoptimized.
 //
-// node --allow-natives-syntax utf8-opt.js
-// node --trace_opt --trace_deopt --allow-natives-syntax utf8-opt.js
+//     node --trace_deopt --allow-natives-syntax utf8-opt.js
 //
+// Reference: https://github.com/simnalamburt/web-checklist/blob/master/v8-microoptimization-guide.md
+//
+// --------
+//
+// This snippet includes source codes written by third parties. The
+// following third party software is included, and carry their own copyright
+// notices and license terms:
+//
+//     MIT License
+//
+//     Copyright (c) 2017 VBChunguk
+//
+//     Permission is hereby granted, free of charge, to any person obtaining a copy
+//     of this software and associated documentation files (the "Software"), to deal
+//     in the Software without restriction, including without limitation the rights
+//     to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//     copies of the Software, and to permit persons to whom the Software is
+//     furnished to do so, subject to the following conditions:
+//
+//     The above copyright notice and this permission notice shall be included in all
+//     copies or substantial portions of the Software.
+//
+//     THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//     IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//     FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//     AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//     LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//     OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//     SOFTWARE.
 "use strict";
 
 function case1(input) {
