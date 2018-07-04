bd7d8ded64508aa82f32d6f65a764d72e4c4b4a4
Hyeon Kim <simnalamburt@gmail.com>
2018-07-04 15:48:39 +0900

    (misc) Remove trailing whitespaces of array.cpp

--- cpp/array.cpp
+++ cpp/array.cpp
@@ -5,14 +5,14 @@ using namespace std;
 int main()
 {
   array<int, 10> List;
-  //     │    └ 배열의 길이 
-  //     │   
+  //     │    └ 배열의 길이
+  //     │
   //     └ int<- 배열 각 원소의 자료형
   //
   // C++ : array<double, 10> Baeyoul;
   //  C  : double[10] Baeyoul;
   // 위의 두개가 같음
-  
+
   cout << "숫자를 열개정도 던져보아라" << endl;
   for (int i=0; i<10; ++i)
   {
