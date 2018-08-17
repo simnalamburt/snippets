6cf1170d860338b7c9734098dfcb622b4586fbc1
Hyeon Kim <simnalamburt@gmail.com>
2018-08-17 20:42:56 +0900

    no-libc.cpp: Add '--nmagic' flag to reduce binary size

--- cpp/no-libc.cpp
+++ cpp/no-libc.cpp
@@ -6,7 +6,7 @@ g++ \
   -fno-exceptions -fno-rtti -fno-stack-protector -fomit-frame-pointer \
   -ffunction-sections -fdata-sections -fno-math-errno -fmerge-all-constants \
   -fno-ident -fno-asynchronous-unwind-tables -fno-unwind-tables \
-  -Wl,--build-id=none,--gc-sections,-z,norelro \
+  -Wl,--build-id=none,--gc-sections,--nmagic,-z,norelro \
   no-libc.cpp &&\
   \
   strip -S --strip-unneeded \
