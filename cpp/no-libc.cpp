d1a42e81b32f0fef0b98cfc7dcc52ef2a813c1d0
Hyeon Kim <simnalamburt@gmail.com>
2018-08-17 17:56:09 +0900

    no-libc.cpp (update)

--- cpp/no-libc.cpp
+++ cpp/no-libc.cpp
@@ -20,63 +20,62 @@ g++ \
 */
 
 namespace {
-  using i8 = signed char;
-  using i16 = signed short int;
-  using i32 = signed int;
-  using i64 = signed long int;
-
-  using u8 = unsigned char;
-  using u16 = unsigned short int;
-  using u32 = unsigned int;
-  using u64 = unsigned long int;
-
-  static_assert(
-    sizeof(i8) == 1 && sizeof(i16) == 2 && sizeof(i32) == 4 && sizeof(i64) == 8 &&
-    sizeof(u8) == 1 && sizeof(u16) == 2 && sizeof(u32) == 4 && sizeof(u64) == 8,
+  // size_t, ssize_t definition
+  using ssize_t = long int;
+  using size_t = unsigned long int;
+  static_assert(sizeof(ssize_t) == 8 && sizeof(size_t) == 8,
     "This program targets only x86_64 GNU/Linux environment.");
 
-  // Reference: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/tree/arch/x86/entry/syscalls/syscall_64.tbl
+  // 64-bit system call numbers
+  //
+  // Reference:
+  //   https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/tree/arch/x86/entry/syscalls/syscall_64.tbl
   constexpr auto SYS_write = 1;
   constexpr auto SYS_exit = 60;
 
-  // Reference: http://man7.org/linux/man-pages/man3/stdin.3.html
+  // The file descriptors associated with the standard I/O streams
+  //
+  // Reference:
+  //   STDIN(3), http://man7.org/linux/man-pages/man3/stdin.3.html
   constexpr auto STDIN_FILENO = 0;
   constexpr auto STDOUT_FILENO = 1;
   constexpr auto STDERR_FILENO = 2;
 
-  // References:
-  // - 148 page of AMD64 ABI Draft 1.0, https://github.com/hjl-tools/x86-psABI/wiki/X86-psABI
-  // - glibc's syscall() function implementation, https://sourceware.org/git/?p=glibc.git;a=blob;f=sysdeps/unix/sysv/linux/x86_64/syscall.S
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wreturn-type"
-  i64 syscall(...) {
-    asm(R"(
-      movq %rdi,    %rax; # syscall number -> rax
-      movq %rsi,    %rdi; # shift arg1 - arg5
-      movq %rdx,    %rsi;
-      movq %rcx,    %rdx;
-      movq %r8,     %r10;
-      movq %r9,     %r8;
-      movq 8(%rsp), %r9;  # arg6 is on the stack.
-      syscall;            # do the system call
-      ret;                # return to caller
-    )");
-  }
-#pragma GCC diagnostic pop
+  // SYSCALL(2) - indirect system call
+  //
+  // Reference:
+  //   148 page of AMD64 ABI Draft 1.0, https://github.com/hjl-tools/x86-psABI/wiki/X86-psABI
+  //   glibc's syscall() function implementation, https://sourceware.org/git/?p=glibc.git;a=blob;f=sysdeps/unix/sysv/linux/x86_64/syscall.S
+  //   SYSCALL(2), http://man7.org/linux/man-pages/man2/syscall.2.html
+  extern "C" [[gnu::naked]] long syscall(long number, ...);
+  asm volatile(R"(
+  syscall:
+    movq %rdi,    %rax; # syscall number -> rax
+    movq %rsi,    %rdi; # shift arg1 - arg5
+    movq %rdx,    %rsi;
+    movq %rcx,    %rdx;
+    movq %r8,     %r10;
+    movq %r9,     %r8;
+    movq 8(%rsp), %r9;  # arg6 is on the stack.
+    syscall;            # do the system call
+    ret;                # return to caller
+  )");
 
-  i64 write(int fd, const void *buf, u64 count) {
+  // WRITE(2) - write to a file descriptor
+  ssize_t write(int fd, const void *buf, size_t count) {
     return syscall(SYS_write, fd, buf, count);
   }
 
+  // _EXIT(2) - terminate the calling process
   [[noreturn]] void _exit(int status) {
     syscall(SYS_exit, status);
-    while (true) { }
+
+    __builtin_unreachable();
   }
 }
 
-extern "C"
-void _start() {
-  constexpr u8 message[] = "Hello, world!\n";
+extern "C" void _start() {
+  constexpr char message[] { 'H', 'e', 'l', 'l', 'o', ',', ' ', 'w', 'o', 'r', 'l', 'd', '!', '\n' };
 
   write(STDOUT_FILENO, message, sizeof message);
   _exit(0);
