b4a85bd7770c5e978f12f4a6e0364dce209ebc07
Hyeon Kim <simnalamburt@gmail.com>
2018-09-23 20:01:00 +0900

    array.php (update)

--- php/array.php
+++ php/array.php
@@ -2,6 +2,20 @@
 
 $a = ['a', 'b', 'c', 'd', 'e'];
 
-echo("$a[3]\n");
-echo($a[3] . "\n");
-echo($a{3} . "\n");
+echo "$a[3]\n";
+echo $a[3] . "\n";
+echo $a{3} . "\n";
+echo "\n";
+echo "\n";
+
+$arr = [
+  'a' => 100,
+  'b' => 10000,
+];
+
+print_r($arr);
+echo "\n";
+
+$arr[c] = 3000;
+
+print_r($arr);
