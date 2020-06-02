13cc068e773de32a4b5aa7399d259543000a7060
Hyeon Kim <simnalamburt@gmail.com>
2020-06-02 18:02:51 +0900

    ieee754.c update

--- c/ieee754.c
+++ c/ieee754.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
-#include <stdint.h>
+#include <inttypes.h>
+
+void f(uint64_t n) {
+  float out = (float)n;
+  uint32_t *ptr = (uint32_t*)&out;
+  printf("0x%08" PRIx32 " = %" PRIu64 ".0\n", *ptr, n);
+}
 
 int main() {
-  for (size_t i = 0; i < 32; ++i) {
-    float out = (float)i;
-    uint32_t *ptr = (uint32_t*)&out;
-    printf("%08x\n", *ptr);
-  }
+  for (uint64_t i = 0; i < 272; ++i) { f(i); }
+  f(8606080);
 }
