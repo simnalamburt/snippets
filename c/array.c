f4f77ed8a686427993c942fb0be5926f83300a89
Hyeon Kim <simnalamburt@gmail.com>
2020-11-26 13:08:30 +0900

    array.c: Designator

--- c/array.c
+++ c/array.c
@@ -1,6 +1,11 @@
-int main()
-{
-  int array[10];
+#include <stdio.h>
+
+int main() {
+  int array[10] = { 1, 2, 3, [6] = 101, 102, 103 };
+  printf("array[10] = { %d", array[0]);
+  for (int i = 1; i < 10; ++i) { printf(", %d", array[i]); }
+  printf(" }\n");
+
   void* p1 = array + 1;
   void* p2 = &array[1];
   return 0;
