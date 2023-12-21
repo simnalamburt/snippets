48c76de8d98db71897408ec1c9cf6bb3293536a5
Hyeon Kim <simnalamburt@gmail.com>
2023-12-21 17:52:37 +0900

    parse_str.php: var_dump instead of print_r

--- php/parse_str.php
+++ php/parse_str.php
@@ -25,10 +25,10 @@ $str = "first=value&arr[]=foo+bar&arr[]=baz&foo[bar]=foobar&test.field=testing";
 // DISCOURAGED
 parse_str($str);
 
-print_r($first); echo "\n";
-print_r($arr); echo "\n";
-print_r($foo); echo "\n";
-print_r($test); echo "\n";
+var_dump($first); echo "\n";
+var_dump($arr); echo "\n";
+var_dump($foo); echo "\n";
+var_dump($test); echo "\n";
 echo "\n\n\n";
 
 // max nested limit
@@ -36,14 +36,21 @@ error_reporting(E_ALL);
 ini_set("display_errors", "stderr");
 $str = "x[x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x]=nesting";
 parse_str($str, $output);
-print_r($output); echo "\n";
+var_dump($output); echo "\n";
 
 // ill input
 parse_str("yo;lo&foo  =   bar%ZZ&yolo + = + swag", $output);
-print_r($output); echo "\n";
+var_dump($output); echo "\n";
 
 parse_str("arr[]=A&arr[]=B&arr[9]=C&arr[]=D&arr[foo]=E&arr[]=F&arr[15.1]=G&arr[]=H", $output);
-print_r($output); echo "\n";
+var_dump($output); echo "\n";
 
-parse_str("=123&[]=123&[foo]=123&[3][var]=123", $output);
-print_r($output); echo "\n";
+parse_str("=123&[]=123&[foo]=123&[3][var]=123", $output); var_dump($output); echo "\n";
+
+parse_str("arr[2][]=sid&arr[2][]=fred", $output); var_dump($output); echo "\n";
+parse_str("arr[2][]=sid&arr[4][]=fred", $output); var_dump($output); echo "\n";
+parse_str("arr[2][]=sid&arr[][4]=fred", $output); var_dump($output); echo "\n";
+parse_str("arr[2][]=sid&arr[][]=fred", $output); var_dump($output); echo "\n";
+parse_str("3.14=adsf&arr[123]=asdf&arr[3.14]=asdf", $output); var_dump($output); echo "\n";
+var_dump(gettype(array_keys($output['arr'])[0])); echo "\n";
+var_dump(gettype(array_keys($output['arr'])[1])); echo "\n";
