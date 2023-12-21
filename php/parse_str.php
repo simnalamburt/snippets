136e9501ba9a71ae90efb01f3a676ea1f758c810
Hyeon Kim <simnalamburt@gmail.com>
2023-12-21 18:23:32 +0900

    parse_str.php

--- php/parse_str.php
+++ php/parse_str.php
@@ -1,5 +1,4 @@
 <?php
-
 // Reference:
 //   https://www.php.net/manual/en/function.parse-str.php
 //
@@ -14,43 +13,31 @@
 //   https://github.com/php/php-src/blob/php-8.3.0/ext/standard/tests/strings/parse_str_memory_error.phpt
 //   https://github.com/php/php-src/blob/php-8.3.0/ext/standard/tests/strings/bug77439.phpt
 
-class TestClass {
-  public $field = "initialvalue";
+// Remove in PHP 8.0
+//
+// class TestClass { public $field = "initialvalue"; }
+// $test = new TestClass();
+// parse_str("first=value&arr[]=foo+bar&arr[]=baz&foo[bar]=foobar&test.field=testing");
+// var_dump($first); echo "\n";
+// var_dump($arr); echo "\n";
+// var_dump($foo); echo "\n";
+// var_dump($test); echo "\n";
+
+function t($str) {
+  parse_str($str, $output);
+  var_dump($output);
+  echo "\n";
 }
 
-$test = new TestClass();
-
-$str = "first=value&arr[]=foo+bar&arr[]=baz&foo[bar]=foobar&test.field=testing";
-
-// DISCOURAGED
-parse_str($str);
-
-var_dump($first); echo "\n";
-var_dump($arr); echo "\n";
-var_dump($foo); echo "\n";
-var_dump($test); echo "\n";
-echo "\n\n\n";
-
+t("first=value&arr[]=foo+bar&arr[]=baz&foo[bar]=foobar&test.field=testing");
 // max nested limit
-error_reporting(E_ALL);
-ini_set("display_errors", "stderr");
-$str = "x[x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x]=nesting";
-parse_str($str, $output);
-var_dump($output); echo "\n";
-
+t("x[x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x]=nesting");
 // ill input
-parse_str("yo;lo&foo  =   bar%ZZ&yolo + = + swag", $output);
-var_dump($output); echo "\n";
-
-parse_str("arr[]=A&arr[]=B&arr[9]=C&arr[]=D&arr[foo]=E&arr[]=F&arr[15.1]=G&arr[]=H", $output);
-var_dump($output); echo "\n";
-
-parse_str("=123&[]=123&[foo]=123&[3][var]=123", $output); var_dump($output); echo "\n";
-
-parse_str("arr[2][]=sid&arr[2][]=fred", $output); var_dump($output); echo "\n";
-parse_str("arr[2][]=sid&arr[4][]=fred", $output); var_dump($output); echo "\n";
-parse_str("arr[2][]=sid&arr[][4]=fred", $output); var_dump($output); echo "\n";
-parse_str("arr[2][]=sid&arr[][]=fred", $output); var_dump($output); echo "\n";
-parse_str("3.14=adsf&arr[123]=asdf&arr[3.14]=asdf", $output); var_dump($output); echo "\n";
-var_dump(gettype(array_keys($output['arr'])[0])); echo "\n";
-var_dump(gettype(array_keys($output['arr'])[1])); echo "\n";
+t("yo;lo&foo  =   bar%ZZ&yolo + = + swag");
+t("arr[]=A&arr[]=B&arr[9]=C&arr[]=D&arr[foo]=E&arr[]=F&arr[15.1]=G&arr[]=H");
+t("=123&[]=123&[foo]=123&[3][var]=123");
+t("arr[2][]=sid&arr[2][]=fred");
+t("arr[2][]=sid&arr[4][]=fred");
+t("arr[2][]=sid&arr[][4]=fred");
+t("arr[2][]=sid&arr[][]=fred");
+t("2=222&3.14=3.14&arr[123]=asdf&arr[3.14]=asdf");
