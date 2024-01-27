8edaaaad94dadde8e28c40bea68f3bad182d2616
Hyeon Kim <simnalamburt@gmail.com>
2024-01-27 14:16:00 +0900

    win32.zig

--- zig/win32.zig
+++ zig/win32.zig
@@ -1,7 +1,15 @@
-// zig build-exe win32.zig -target x86_64-windows
+// zig build-exe win32.zig -target x86_64-windows --subsystem windows
 
 const std = @import("std");
+const w = std.os.windows;
 
-pub fn main() void {
-    _ = std.os.windows.user32.MessageBoxA(null, "hello", "title", 0);
+pub fn main() !void {
+    _ = w.user32.MessageBoxA(null, "hello", "title", 0);
 }
+
+// You can use wWinMain as well
+//
+// pub export fn wWinMain(_: w.HINSTANCE, _: ?w.HINSTANCE, _: w.PWSTR, _: c_int) callconv(w.WINAPI) c_int {
+//     _ = w.user32.MessageBoxA(null, "hello", "title", 0);
+//     return 0;
+// }
