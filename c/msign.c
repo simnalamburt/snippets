acde9a32b292d099a4118913c043e41df77b8124
Hyeon Kim <simnalamburt@gmail.com>
2024-05-16 21:25:47 +0900

    msign.c

--- c/msign.c
+++ c/msign.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include <stdint.h>
 
+float msign2(float f) {
+  if (f >= 0.0f) {
+    return 1.0f;
+  } else {
+    return -1.0f;
+  }
+}
+
 float msign(float f) {
   uint32_t i = *(uint32_t*)(&f);
   uint32_t ret = (i & 0x80000000u) | 0x3f800000;
@@ -8,12 +16,17 @@ float msign(float f) {
 }
 
 int main() {
-  printf("%f\n", msign(+0.0f));
-  printf("%f\n", msign(-0.0f));
-  printf("%f\n", msign(+1.0f));
-  printf("%f\n", msign(-1.0f));
-  printf("%f\n", msign(+3.14f));
-  printf("%f\n", msign(-3.14f));
-  printf("%f\n", msign(+3.14f));
-  printf("%f\n", msign(-3.14f));
+  float sum;
+
+  sum = 0.0f;
+  for (long i = 0; i < 10000000000; ++i) {
+    sum += msign((float)i);
+  }
+  printf("sum = %f\n", sum);
+
+  sum = 0.0f;
+  for (long i = 0; i < 10000000000; ++i) {
+    sum += msign2((float)i);
+  }
+  printf("sum = %f\n", sum);
 }
